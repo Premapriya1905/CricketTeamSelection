b41fe585e18aee8bd60a1d340c21d3e3810d5bfc
Premapriya <premapriya1905@gmail.com>
2025-07-05 10:01:10 +0530

    commit2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import "./App.css"
 import HomePage from "./components/HomePage"
 import GameRoom from "./components/GameRoom"
 
-const socket = io("cricketteamselection-production.up.railway.app")
+const socket = io("http://cricketteamselection-production.up.railway.app")
 
 function App() {
   const [currentView, setCurrentView] = useState("home")
